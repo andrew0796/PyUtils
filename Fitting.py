d5a8377ee3dee61afd7dedee6238f09cf18dd552
andrew0796 <andrew0796@gmail.com>
2018-03-02 09:34:00 -0500

    Added Gaussian to Fitting

--- Fitting.py
+++ Fitting.py
@@ -4,22 +4,50 @@
 Utilities for fitting data in python
 '''
 
+# TODO
+#   Add Jacobian support
+#   Have built in functions called by strings instead of passed as functions
+#   Return a figure of merit, ie chi squared (per DOF) or likelihood or something
+
 import scipy.odr as odr
 from scipy.optimize import curve_fit
 import numpy as np
 import pandas as pd
 
-def linear(B, x):
-    ''' simple linear model '''
+def linearODR(B, x):
+    ''' 
+    simple linear model to be used with scipy.ODR
+    y = B[0]+B[1]*x
+    '''
     return B[0]+B[1]*x
+
+def linearCF(x, b,a):
+    '''
+    simple linear model to be used with scipy.optimize.curve_fit
+    y = a*x+b
+    '''
+    return a*x+b
+
+def gaussianODR(B, x):
+    ''' 
+    Gaussian model to be used with scipy.ODR
+    y = B[0]*exp(-(x-B[1])^2/(2*B[2]^2)+B[3]
+    '''
+    return B[0]*np.exp(-np.square(B[1]-x)/(2*np.square(B[2])))+B[3]
+
+def gaussianCF(x, A,x0,s,C):
+    '''
+    Gaussian model to be used with scipy.optimize.curve_fit
+    y = A*exp(-(x-x0)^2/(2*s^2))+C
+    '''
+    return A*np.exp(-np.square(x-x0)/(2*np.square(s)))+C
 
 def FitData(f, x,y, dx=None,dy=None, guess=None):
     '''
     fit data y to independent variable x with model f (ie f(x)=y)
-    
-    for now just return the fitted parameters, and their errors
-    in the future I'd like to also return some figure of merit, for instance chi squared
 
+    for now just return the fitted parameters, and their errors
+    
     dx and dy are errors on x and y (look up scipy docs for more)
     guess is a collection (list, array, etc) of guess parameters
 
